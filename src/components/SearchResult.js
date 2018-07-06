92ca7612c0dc0c008c4b4aa6abf0116fd9cd4a00
15110110-VoThanhTruongQuang <37280106+15110110-VoThanhTruongQuang@users.noreply.github.com>
2018-07-06 23:14:14 +0700

    add lowercase search

---
+++ src/components/SearchResult.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom'
+import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom'
+import Header from './header'
+import './../styles/setting.css'
+
+class SearchResult extends Component {
+    
+}
+export default SearchResult
