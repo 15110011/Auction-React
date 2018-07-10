7e3224b2bce56510e9fda7e744a9e69cf3004618
15110110-VoThanhTruongQuang <37280106+15110110-VoThanhTruongQuang@users.noreply.github.com>
2018-07-10 11:32:04 +0700

    check status

--- src/components/AdminPendingItems.js
+++ src/components/AdminPendingItems.js
@@ -6,6 +6,9 @@ import '../styles/styles.css'
 import classnames from 'classnames';
 import '../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import _ from 'lodash'
+import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom'
+
+import DashBoard from './dashboard'
 
 class AdminPendingItems extends Component {
     constructor(props) {
@@ -14,71 +17,77 @@ class AdminPendingItems extends Component {
         this.state = {
             items: [],
             itemsStatus: [],
-            selectedCount:0,
+            selectedCount: 0,
+            isAccept: null
         }
     }
 
 
     componentDidMount() {
-        console.log("DIDMOUNT")
         fetch('/api/v1/pendingitems/').then(res => res.json())
             .then(items => {
                 if (!items.error) {
-                    console.log(items)
-                    this.setState({ items: items.pendingItems, itemsStatus:items.pendingItems.map(i=>{
-                        return {id:i.id,selected:false}
-                    }) })
+                    this.setState({
+                        items: items.pendingItems, itemsStatus: items.pendingItems.map(i => {
+                            return { id: i.id, selected: false }
+                        })
+                    })
                 }
             })
     }
 
-    onSelectCheckBox(e){
-        let index = _.findIndex(this.state.itemsStatus,i=>{
+    onSelectCheckBox(e) {
+        let index = _.findIndex(this.state.itemsStatus, i => {
             return i.id === +e.target.value
         })
-        console.log(index,e.target.value)
-        if(index>-1){
+        console.log(index, e.target.value)
+        if (index > -1) {
             let cloneSelected = this.state.itemsStatus.slice()
             cloneSelected[index].selected = !cloneSelected[index].selected
-            this.setState({itemsStatus:cloneSelected})
-            if(cloneSelected[index].selected){
-                let newSelectedCount =this.state.selectedCount+1
-                this.setState({selectedCount:newSelectedCount})
+            this.setState({ itemsStatus: cloneSelected })
+            if (cloneSelected[index].selected) {
+                let newSelectedCount = this.state.selectedCount + 1
+                this.setState({ selectedCount: newSelectedCount })
             }
-            else{
-                let newSelectedCount =this.state.selectedCount-1
+            else {
+                let newSelectedCount = this.state.selectedCount - 1
 
-                this.setState({selectedCount:newSelectedCount})
-                
+                this.setState({ selectedCount: newSelectedCount })
+
             }
         }
     }
-    onClickButton(e){
+    onClickButton(e) {
         let button = e.currentTarget
         console.log(button.value)
-        
-        if(button.value)
-        {
-            let selectedItems = _.filter(this.state.itemsStatus,{selected:true})
+
+        if (button.value) {
+            let selectedItems = _.filter(this.state.itemsStatus, { selected: true })
             fetch('/api/v1/pendingitems/', {
                 method: 'PATCH',
                 body: JSON.stringify({
-                    type:button.value,
+                    type: button.value,
                     selectedItems,
                     adminId: this.props.userId
                 })
             }).then(res => res.json())
-            .then(res => {
-                if(!res.error){
-                    let remainPending = _.filter(this.state.items,i=>{
+                .then(res => {
+                    if (!res.error) {
+                        console.log(res.isAccept)
+                        let remainPending = _.filter(this.state.items, i => {
 
-                        return _.findIndex(selectedItems,selected=>{ return i.id === selected.id })===-1
-                    })
-                    this.setState({items:remainPending, itemsStatus:remainPending.map(i=>{
-                        return {id:i.id,selected:false}
-                    }),selectedCount:0})
-                }
-            })
+                            return _.findIndex(selectedItems, selected => { return i.id === selected.id }) === -1
+                        })
+
+                        this.setState({
+                            isAccept: res.isAccept,
+                            items: remainPending, itemsStatus: remainPending.map(i => {
+                                return { id: i.id, selected: false }
+                            }), selectedCount: 0
+                        })
+                        console.log(this.state.itemsStatus)
+                    }
+                })
         }
     }
     render() {
@@ -86,64 +95,65 @@ class AdminPendingItems extends Component {
         return (
             <Row>
                 <Col sm="12">
-                        <table className="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Item ID</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Price</th>
-                                    <th scope="col">Quantity</th>
-                                    <th scope="col">Category</th>
-                                    <th scope="col">Owner</th>
-                                    <th scope="col">Detail</th>
-                                    <th scope="col">
-                                        <div className="btn-group">
-                                            <button type="button" className="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                Action on {this.state.selectedCount} {this.state.selectedCount<=1?'item':'items'}
-                                            </button>
-                                            <div className="dropdown-menu dropdown-menu-right">
-                                                <button className="dropdown-item" name="action" value="ACCEPT" onClick={e=>this.onClickButton(e)}>Accept</button>
-                                                <button className="dropdown-item" name="action" value="REJECT" onClick={e=>this.onClickButton(e)}>Reject</button>
-                                            </div>
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Item ID</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Category</th>
+                                <th scope="col">Owner</th>
+                                <th scope="col">Detail</th>
+                                <th scope="col">
+                                    <div className="btn-group">
+                                        <button type="button" className="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Action on {this.state.selectedCount} {this.state.selectedCount <= 1 ? 'item' : 'items'}
+                                        </button>
+                                        <div className="dropdown-menu dropdown-menu-right">
+                                            <button className="dropdown-item" name="action" value="ACCEPT" onClick={e => this.onClickButton(e)}>Accept</button>
+                                            <button className="dropdown-item" name="action" value="REJECT" onClick={e => this.onClickButton(e)}>Reject</button>
                                         </div>
-                                        
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    this.state.items.map(item => {
-                                        return (
-                                            <tr className="fixprop" key={item.id}>
-                                                <td>
-                                                    {item.id}
-                                                </td>
-                                                <td>{item.name}</td>
-                                                <td>{item.currentPrice}</td>
-                                                <td>{item.quantity}</td>
-                                                <td>{item.categoryName}</td>
-                                                <td>{item.userId}</td>
-                                                <td>
-                                                    <div className="edit-del">
-                                                        <button className="btn btn-info" style={{ color: '#1d93c1' }}><i className="fas fa-eye"></i></button>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <FormGroup check>
-                                                        <Label check />
-                                                        <Input type="checkbox"  value={item.id} onChange={e=>this.onSelectCheckBox(e)}/>{' '}
-                                                    </FormGroup>
-                                                </td>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </tbody>
-                        </table>
+                                    </div>
+
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.items.map(item => {
+                                    return (
+                                        <tr className="fixprop" key={item.id}>
+                                            <td>
+                                                {item.id}
+                                            </td>
+                                            <td>{item.name}</td>
+                                            <td>{item.currentPrice}</td>
+                                            <td>{item.quantity}</td>
+                                            <td>{item.categoryName}</td>
+                                            <td>{item.userId}</td>
+                                            <td>
+                                                <div className="edit-del">
+                                                    <button className="btn btn-info" style={{ color: '#1d93c1' }}><i className="fas fa-eye"></i></button>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <FormGroup check>
+                                                    <Label check />
+                                                    <Input type="checkbox" value={item.id} onChange={e => this.onSelectCheckBox(e)} />{' '}
+                                                </FormGroup>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
 
                 </Col>
             </Row>
         )
+
     }
 }
 
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -141,6 +141,7 @@ class DashBoard extends Component {
             .then(res => res.json())
             .then((res) => {
                 if (res.item) {
+
                     this.setState({ isAdded: true, name: '', quantity: '', currentPrice: '', details: '', count: 0, editorState: '' })
                     this.getItem()
                     setTimeout(() => {
@@ -225,7 +226,8 @@ class DashBoard extends Component {
             })
             .then(items => {
                 console.log(items)
-                this.setState({ items: items.findItem, renderedItems: items.findItem.slice(0, 4), total: items.findItem.length })
+
+                this.setState({ items: items.findItem, renderedItems: items.findItem.slice(0, 4), total: items.findItem.length, page: 1 })
             })
 
     }
@@ -236,6 +238,7 @@ class DashBoard extends Component {
     handleChange(e) {
         this.setState({ categoriesId: e.target.value })
     }
+
     render() {
         const { currentNote } = this.props
         const { page, total, renderedItems } = this.state
@@ -246,6 +249,7 @@ class DashBoard extends Component {
                 </div>
             )
         }
+
         return (
             <div className="container">
                 <div className="dashboard">
@@ -440,7 +444,27 @@ class DashBoard extends Component {
                                                             <button className="btn btn-danger mx-2" onClick={this.handleDelete} value={item.id}><i className="far fa-trash-alt"></i></button>
                                                         </div>
                                                     </td>
-                                                    <td><span className="badge badge-pill badge-success light-word">Accepted</span></td>
+                                                    {
+                                                        item.isAccept==1  && (
+                                                            <td><span className="badge badge-pill badge-success light-word">Accepted</span></td>
+
+                                                        )
+                                                        
+                                                    }
+                                                    {
+                                                        item.isAccept === 0 && (
+                                                            <td><span className="badge badge-pill badge-danger light-word">Rejected</span></td>
+
+                                                        )
+                                                        
+                                                    }
+                                                    {
+                                                        item.isAccept === null && (
+                                                            <td><span className="badge badge-pill badge-secondary light-word">Pending</span></td>
+
+                                                        )
+                                                        
+                                                    }
                                                     {/* <td><span className="badge badge-pill badge-danger light-word">Rejected</span></td>
                                                     <td><span className="badge badge-pill badge-secondary light-word">Pending</span></td> */}
                                                 </tr>
