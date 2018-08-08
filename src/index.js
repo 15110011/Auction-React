c6ed43a90273c6a1bacbe23df29bd4cda8384cdc
Đặng Hồ Vũ <dangvanminh09@gmail.com>
2018-08-08 16:24:14 +0700

    fix white screen of dead

--- src/index.js
+++ src/index.js
@@ -4,25 +4,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {  BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 
 
 window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1603931246371721',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v3.0'
-    });
-      
-    FB.AppEvents.logPageView();   
-    ReactDOM.render(
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-        , document.getElementById('root'));
-    registerServiceWorker();
-          
-  };
+  FB.init({
+    appId      : '1603931246371721',
+    cookie     : true,
+    xfbml      : true,
+    version    : 'v3.0'
+  });
     
+  FB.AppEvents.logPageView();   
+
+  const app = (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+  
+  ReactDOM.render(app, document.getElementById('root'));
+
+};
+
+registerServiceWorker();
