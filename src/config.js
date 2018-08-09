a228899226c183cd8af28a903c5dc8427561d7d3
Đặng Hồ Vũ <dangvanminh09@gmail.com>
2018-08-09 11:41:38 +0700

    fix API_URL

--- src/config.js
+++ src/config.js
@@ -6,12 +6,12 @@ const UPDATED_MODE = 2002
 const DELETED_MODE = 2003
 
 const API_URL = window.location.host.includes('localhost') 
-	? 'http://localhost:3000' 
-	: window.location.toString()
+	? 'http://localhost:3000'
+	: window.location.toString().slice(0, -1)
 
 const WS_URL = window.location.host.includes('localhost') 
 	? 'http://localhost:1337' 
-	: window.location.toString()
+	: window.location.toString().slice(0, -1)
 
 export {
   GUEST_STATUS, LOADED_LOGIN_STATUS, LOADING_LOGIN_STATUS, CREATED_MODE, UPDATED_MODE, DELETED_MODE,
