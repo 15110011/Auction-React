7c31cb3a5266e2316bd17b0d0bd9c886fdf88b26
Đặng Hồ Vũ <dangvanminh09@gmail.com>
2018-08-21 00:24:54 +0700

    add pagination

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import dateFns from 'date-fns'
 import { Link } from 'react-router-dom'
+import Pagination from './Pagination'
+import '../styles/styles.css'
 
 
 class Comments extends Component {
@@ -8,8 +10,10 @@ class Comments extends Component {
         super(props)
         this.state = {
             comments: [],
-            newCommentContent: ''
-
+            newCommentContent: '',
+            pageComment: 1,
+            renderedComments: [],
+            commentPerPage: 3
         }
         this.onSubmitComment = this.onSubmitComment.bind(this)
     }
@@ -18,11 +22,25 @@ class Comments extends Component {
             fetch(`/api/v1/items/${this.props.itemId}/comments`)
                 .then(res => res.json())
                 .then(comments => {
-                    if (!comments.error)
+                    if (!comments.error) {
                         this.setState({ comments: comments.comments })
+                        const renderedComments = 
+                            comments.comments.slice((this.state.pageComment - 1) * 
+                            this.state.commentPerPage, (this.state.pageComment - 1) * 
+                            this.state.commentPerPage + this.state.commentPerPage)
+                        this.setState({ renderedComments })
+                    }
                 })
         }
     }
+    handleCommentPageChange = (page) => {
+        const renderedComments = 
+            this.state.comments.slice((page - 1) 
+            * this.state.commentPerPage, (page - 1) 
+            * this.state.commentPerPage + this.state.commentPerPage)
+        this.setState({ pageComment: page, renderedComments })
+    }
+
     onSubmitComment(e) {
         this.setState({ newCommentContent: '' })
         e.preventDefault()
@@ -36,47 +54,70 @@ class Comments extends Component {
             } else {
                 let cloneComments = this.state.comments.slice()
                 cloneComments.unshift(res.newComment)
-                this.setState({ comments: cloneComments })
+                this.setState({ comments: cloneComments }, () => {
+                    this.handleCommentPageChange(1)
+
+                })
             }
         })
 
     }
     render() {
+        const { pageComment, commentPerPage, renderedComments, comments } = this.state
         return (
-            <div>
-                {(this.props.startedAt !== 0) &&
-                    <div>
-                        <h4 className="light-word">Leave a comment here:</h4>
-                        <form onSubmit={this.onSubmitComment}>
-                            <textarea name="content" id="" cols="96" rows="5" style={{ resize: 'none' }} onChange={e => {
-                                this.setState({ newCommentContent: e.target.value })
-                            }} defaultValue={this.state.newCommentContent}></textarea>
-                            <button type="submit" className="btn btn-success mb-2" style={{ marginLeft: '-78px', marginTop: '70px' }} >Submit</button>
-                        </form>
-                    </div>
-                }
-
-                <ul>
-                    {this.state.comments.map((cmt, i) => {
-                        return (
-                            <div className="cmt light-word pt-2" style={{ marginLeft: '-40px' }} key={i}>
-                                <div className="question italic-word">
-                                    <li style={{ backgroundColor: '#F1F1F1', borderRadius: '10px', listStyleType: 'none', padding: '10px 10px' }} key={cmt.id}>
-                                        {cmt.content}
-                                        <div className="user-question">
-                                            <small style={{ color: '#7b7171' }}>by <Link to="#">{cmt.user.userName}</Link> - {dateFns.format(cmt.createdAt, 'hh:mm DD/MM/YYYY')}</small>
-                                        </div>
-                                        <hr />
-                                        <div className="answer italic-word">
-                                            <small>Have a nice day - <strong>Aution Blocha Team</strong></small>
-                                        </div>
-                                    </li>
+            <Fragment>
+                <div className="comment-titles pt-3">
+                    <h3>Comments</h3>
+                </div>
+                <hr style={{ width: '825px', marginLeft: '-16px' }} />
+                <div className="comment-content">
+                    <div className="comment-block">
+                        <div>
+                            {(this.props.startedAt !== 0) &&
+                                <div>
+                                    <h4 className="light-word">Leave a comment here:</h4>
+                                    <form onSubmit={this.onSubmitComment}>
+                                        <textarea name="content" id="" cols="96" rows="5" style={{ resize: 'none' }} onChange={e => {
+                                            this.setState({ newCommentContent: e.target.value })
+                                        }} defaultValue={this.state.newCommentContent}></textarea>
+                                        <button type="submit" className="btn btn-success mb-2" style={{ marginLeft: '-78px', marginTop: '70px' }} >Submit</button>
+                                    </form>
                                 </div>
-                            </div>
-                        )
-                    })}
-                </ul>
-            </div>
+                            }
+
+                            <ul>
+                                {renderedComments.map((cmt, i) => {
+                                    return (
+                                        <div className="cmt light-word pt-2" style={{ marginLeft: '-40px' }} key={i}>
+                                            <div className="question italic-word">
+                                                <li style={{ backgroundColor: '#F1F1F1', borderRadius: '10px', listStyleType: 'none', padding: '10px 10px' }} key={cmt.id}>
+                                                    {cmt.content}
+                                                    <div className="user-question">
+                                                        <small style={{ color: '#7b7171' }}>by <Link to="#">{cmt.user.userName}</Link> - {dateFns.format(cmt.createdAt, 'hh:mm DD/MM/YYYY')}</small>
+                                                    </div>
+                                                    <hr />
+                                                    <div className="answer italic-word">
+                                                        <small>Have a nice day - <strong>Aution Blocha Team</strong></small>
+                                                    </div>
+                                                </li>
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-end">
+                    {/*pagin*/}
+                    <Pagination
+                        margin={2}
+                        page={pageComment}
+                        count={Math.ceil(comments.length / commentPerPage)}
+                        onPageChange={this.handleCommentPageChange}
+                    />
+                </div>
+            </Fragment>
         )
     }
 }
--- src/components/itemdetail.js
+++ src/components/itemdetail.js
@@ -7,7 +7,7 @@ import RatingList from './RatingList'
 import dateFns from 'date-fns'
 import BidInput from './BidInput'
 import NumberFormat from 'react-number-format';
-import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import Pagination from './Pagination'
 import Web3 from 'web3';
 
 import AuctionBid from '../contracts/AuctionBid.json';
@@ -36,7 +36,10 @@ class ItemDetail extends Component {
             waitForMining: false,
             deployContract: true,
             startBidding: false,
-            ended: false
+            ended: false,
+            pageBid: 1,
+            renderedBids: [],
+            bidPerPage: 3
         }
         if (typeof this.web3 !== 'undefined') {
             this.web3Provider = this.web3.currentProvider
@@ -109,12 +112,24 @@ class ItemDetail extends Component {
     }
     onReceiveRoomMessage(newBid) {
         // console.log(this.state.itemDetail)
-        let { itemDetail } = this.state
-        if (itemDetail) {
+        // let { itemDetail } = this.state
+        // if (itemDetail) {
+        //     itemDetail.bids.unshift(newBid)
+        //     itemDetail.currentPrice = itemDetail.bids[0].currentPrice
+        //     let nextStep = itemDetail.bids[0].currentPrice * 0.5
+        //     this.setState({ itemDetail, step: nextStep, currentBidding: newBid.currentPrice + nextStep })
+        // }
+        //??????????????????????????????????????
+        let { bidders, itemDetail } = this.state
+        if (bidders) {
+            bidders.unshift(newBid)
             itemDetail.bids.unshift(newBid)
-            itemDetail.currentPrice = itemDetail.bids[0].currentPrice
-            let nextStep = itemDetail.bids[0].currentPrice * 0.5
-            this.setState({ itemDetail, step: nextStep, currentBidding: newBid.currentPrice + nextStep })
+            itemDetail.currentPrice = bidders[0].currentPrice
+
+            let nextStep = bidders[0].currentPrice * 0.5
+            this.setState({ itemDetail, bidders, step: nextStep, currentBidding: newBid.currentPrice + nextStep }, ()=>{
+                this.handleBidPageChange(1)
+            })
         }
     }
     getBidder = () => {
@@ -131,7 +146,9 @@ class ItemDetail extends Component {
                 }
                 Promise.all(promises).then((e) => {
 
-                    this.setState({ bidders: e.reverse() })
+                    this.setState({ bidders: e.reverse() }, () => {
+                        this.handleBidPageChange(1)
+                    })
                 })
             })
         }
@@ -384,8 +401,16 @@ class ItemDetail extends Component {
 
             })
     }
+
+    handleBidPageChange= (page) => {
+        const renderedBids = this.state.bidders.slice((page - 1) * this.state.bidPerPage, 
+            (page - 1) * this.state.bidPerPage + this.state.bidPerPage)
+        this.setState({ pageBid: page, renderedBids })
+    }
+
     render() {
-        const { current, itemDetail, images, loading, isApproved, getMetaMask, sendTransaction, waitForMining } = this.state
+        const { current, itemDetail, images, loading, isApproved, getMetaMask,
+            sendTransaction, waitForMining, bidPerPage, pageBid, bidders } = this.state
         if (loading) {
             return (
                 <div role="alert" style={{ marginTop: '75px' }}>
@@ -548,7 +573,7 @@ class ItemDetail extends Component {
                                         </thead>
                                         <tbody className="light-word">
                                             {
-                                                this.state.bidders.map((bid, i) => {
+                                                this.state.renderedBids.map((bid, i) => {
                                                     return (
                                                         <tr key={i}>
                                                             <td>{bid.address}</td>
@@ -563,39 +588,12 @@ class ItemDetail extends Component {
                                         </tbody>
                                     </table>
                                     <div className="d-flex justify-content-center">
-                                        <Pagination aria-label="Page navigation example">
-                                            <PaginationItem>
-                                                <PaginationLink previous href="#" />
-                                            </PaginationItem>
-                                            <PaginationItem>
-                                                <PaginationLink href="#">
-                                                    1
-                                                </PaginationLink>
-                                            </PaginationItem>
-                                            <PaginationItem>
-                                                <PaginationLink href="#">
-                                                    2
-                                                </PaginationLink>
-                                            </PaginationItem>
-                                            <PaginationItem>
-                                                <PaginationLink href="#">
-                                                    3
-                                                </PaginationLink>
-                                            </PaginationItem>
-                                            <PaginationItem>
-                                                <PaginationLink href="#">
-                                                    4
-                                                </PaginationLink>
-                                            </PaginationItem>
-                                            <PaginationItem>
-                                                <PaginationLink href="#">
-                                                    5
-                                                </PaginationLink>
-                                            </PaginationItem>
-                                            <PaginationItem>
-                                                <PaginationLink next href="#" />
-                                            </PaginationItem>
-                                        </Pagination>
+                                        <Pagination
+                                            margin={2}
+                                            page={pageBid}
+                                            count={Math.ceil(bidders.length / bidPerPage)}
+                                            onPageChange={this.handleBidPageChange}
+                                        />
                                     </div>
                                 </div>
                                 <hr />
@@ -610,50 +608,18 @@ class ItemDetail extends Component {
                                 {(this.props.userId !== '' && itemDetail) && <RatingList userId={this.props.userId} itemDetail={itemDetail} itemId={this.props.match.params.id} isApproved={isApproved}></RatingList>}
                             </div>
                             <div className="col-md-12 items-info mt-2">
-                                <div className="comment-titles pt-3">
+                                {/* <div className="comment-titles pt-3">
                                     <h3>Comments</h3>
                                 </div>
                                 <hr style={{ width: '825px', marginLeft: '-16px' }} />
                                 <div className="comment-content">
-                                    <div className="comment-block">
-                                        <Comments itemId={this.props.match.params.id} userId={this.props.userId} startedAt={this.state.itemDetail.startedAt} io={this.props.io}></Comments>
-                                    </div>
+                                    <div className="comment-block"> */}
+                                <Comments itemId={this.props.match.params.id} userId={this.props.userId} startedAt={this.state.itemDetail.startedAt} io={this.props.io}></Comments>
+                                {/* </div>
                                 </div>
-                                <div className="d-flex justify-content-end">
-                                    <Pagination aria-label="Page navigation example">
-                                        <PaginationItem>
-                                            <PaginationLink previous href="#" />
-                                        </PaginationItem>
-                                        <PaginationItem>
-                                            <PaginationLink href="#">
-                                                1
-                                                </PaginationLink>
-                                        </PaginationItem>
-                                        <PaginationItem>
-                                            <PaginationLink href="#">
-                                                2
-                                                </PaginationLink>
-                                        </PaginationItem>
-                                        <PaginationItem>
-                                            <PaginationLink href="#">
-                                                3
-                                                </PaginationLink>
-                                        </PaginationItem>
-                                        <PaginationItem>
-                                            <PaginationLink href="#">
-                                                4
-                                                </PaginationLink>
-                                        </PaginationItem>
-                                        <PaginationItem>
-                                            <PaginationLink href="#">
-                                                5
-                                                </PaginationLink>
-                                        </PaginationItem>
-                                        <PaginationItem>
-                                            <PaginationLink next href="#" />
-                                        </PaginationItem>
-                                    </Pagination>
-                                </div>
+                                <div className="d-flex justify-content-end"> */}
+                                {/*pagin*/}
+                                {/* </div> */}
                             </div>
                         </div>
                         <div className="col-sm-3">
